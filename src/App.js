01b1c6bf2783cbad02f0d632a9d41cad4070fe39
Allyn Alda <Teddy@om.home>
2019-10-01 17:25:17 +0200

    added accordion

--- src/App.js
+++ src/App.js
@@ -2,10 +2,43 @@ import React from 'react';
 import './App.css';
 import 'accordion-stencil-component';
 
+
 function App() {
+
+  const array = [
+    {
+      label: 'Accordion 1',
+      description: 'Lorem ipsum',
+      color: '#439ECA',
+      width: '300px'
+    },
+    {
+      label: 'Accordion 2',
+      description: 'Lorem ipsum',
+      color: '#7EC74A',
+      width: '300px'
+    },
+    {
+      label: 'Accordion 3',
+      description: 'Lorem ipsum',
+      color: '#F8CD41',
+      width: '300px'
+    }
+  ]
   return (
     <div className="App">
-      <my-accordion label="Hello" width="200px"></my-accordion>
+      {
+        array.map((array) => {
+          return (
+            <my-accordion 
+            label={array.label}
+            description={array.description}
+            color={array.color}
+            width={array.width}>
+            </my-accordion>
+          )
+        })
+      }
     </div>
   );
 }
